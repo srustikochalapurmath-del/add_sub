cecde9e94d46f4917f73ccad69c74c8ef9d565fa
srustikochalapurmath-del <srustikochalapurmath@gmail.com>
2025-09-30 12:06:31 +0530

    Create add_sub.py

---
+++ add_sub.py
@@ -0,0 +1,13 @@
+# Program to perform addition and subtraction using user input
+
+# take input from user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# perform addition and subtraction
+addition = a + b
+subtraction = a - b
+
+# display results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
